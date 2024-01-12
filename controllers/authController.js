097652929a22ff5e93df2827901cf0379f1f1c41
Geoffrey <matiesogeoffrey@gmail.com>
2024-01-12 23:31:56 +0300

    Validation failed

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,11 @@
 const User = require('../models/User');
 
+// handle error 
+const handleErrors = (err) => {
+    console.log(err.message, err.code);
+
+}
+
 module.exports.signup_get = async (req, res) => {
     res.render('signup');
 }
@@ -20,7 +26,8 @@ module.exports.signup_post = async (req, res) => {
         res.status(201).json(user);
     }
     catch(err){
-        console.log(err);
+        // console.log(err);
+        handleErrors(err);
         res.status(400).send('error, user not created');
     }
 
