23832a7fe34744a17f9fabafd59bae94cc2b84c1
Geoffrey <matiesogeoffrey@gmail.com>
2024-01-15 22:47:18 +0300

    Cookies primer done

--- app.js
+++ app.js
@@ -2,13 +2,15 @@ require('dotenv').config(); // to use .env file
 const express = require('express');
 const mongoose = require('mongoose');
 const authRoutes = require('./routes/authRoutes');
+const cookieParser = require('cookie-parser'); // to use cookies 
+
 
 const app = express();
 
 // middleware
 app.use(express.static('public'));
 app.use(express.json()); // to handle json data  // express.json() is a method inbuilt in express to recognize the incoming Request Object as a JSON Object. This method is called as a middleware in your application using the code: app. use(express. json());
-
+app.use(cookieParser()); // to use cookies
 // view engine
 app.set('view engine', 'ejs');
 
@@ -22,4 +24,24 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCr
 // routes
 app.get('/', (req, res) => res.render('home'));
 app.get('/smoothies', (req, res) => res.render('smoothies'));
-app.use(authRoutes)
+app.use(authRoutes);
+
+// cookies 
+app.get('/set-cookies', (req, res) => {
+
+  // res.setHeader('Set-Cookie', 'newUser=true'); // key value pair
+
+  res.cookie('newUser', false); // key value pair
+  res.cookie('isEmployee', true, { maxAge: 1000 * 60 * 60 * 24, httpOnly: true }); // key value pair
+
+  res.send('you got the cookies!');
+
+});
+
+
+app.get('/read-cookies', (req, res) => {
+  const cookies = req.cookies;
+  console.log(cookies);
+
+  res.json(cookies)
+});
